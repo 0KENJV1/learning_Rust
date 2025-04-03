6acc2fbe84afff703d2100d7719afde635ad2ad3
KENJV <192631410+0KENJV1@users.noreply.github.com>
2025-04-03 17:30:45 -0500

    exp(main):practica de un juego de adivinanzas terminado

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
+use rand::Rng;
+use std::{cmp::Ordering, io};
 
+fn main() {
+    println!("----ADIVINA EL NUMERO!----");
+
+    let secret_number = rand::rng().random_range(1..=100);
+
+    println!("secret number: {}", secret_number);
+
+    loop {
+        println!("----------------------------------------");
+        println!("porfavor introduce tu valor a adivinar: ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("no se pudo leer el valor");
+
+        println!("tu valor adivinado es: {}", guess);
+
+        let guess: i32 = guess.trim().parse().expect("Error");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("el numero es pequeño, sigue adivinando!"),
+            Ordering::Greater => println!("el numero es grande, sigue adivinando!"),
+            Ordering::Equal => {
+                println!("Ganastes has adivinado el numero!!!");
+                break;
+            }
+        }
+    }
 }
